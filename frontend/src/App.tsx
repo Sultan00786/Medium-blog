d5d1650d7a5eb866e10b013da9076d6ac2dd216d
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-01-23 23:49:42 +0530

    deployment changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Blog from "./pages/Blog.tsx";
-import Signin from "./pages/Signin";
-import Singup from "./pages/Singup";
 import Blogs from "./pages/Blogs.tsx";
 import Publish from "./pages/Publish.tsx";
-import { useEffect } from "react";
 import Root from "./pages/Root.tsx";
+import Signin from "./pages/Signin";
+import Singup from "./pages/Singup";
 
 function App() {
    return (
